906b4272bc1d0ebe8fcadd9e00aa6caf4fa844df
Alex Young <alexyoung1992@gmail.com>
2020-03-08 12:23:43 -0400

    chore(graphql): file reorg

--- Lotus/Services/Network.swift
+++ Lotus/Services/Network.swift
@@ -9,7 +9,7 @@ class Network {
   static let shared = Network()
   private lazy var config: Config = {
     guard
-      let path = Bundle.main.path(forResource: "Config", ofType: "plist"),
+      let path = Bundle.main.path(forResource: "Apollo", ofType: "plist"),
       let xml = FileManager.default.contents(atPath: path),
       let config = try? PropertyListDecoder().decode(Config.self, from: xml)
     else {
