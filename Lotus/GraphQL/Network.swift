1af54e06d12b47fd8f55975f571e838fa6b82519
Alex Young <alexyoung1992@gmail.com>
2020-03-08 12:04:15 -0400

    feat(Network): Apollo client singleton

---
+++ Lotus/GraphQL/Network.swift
@@ -0,0 +1,21 @@
+import Apollo
+
+private struct Config: Codable {
+  var apiUrl: String
+}
+
+class Network {
+
+  static let shared = Network()
+  private lazy var config: Config = {
+    guard
+      let path = Bundle.main.path(forResource: "Config", ofType: "plist"),
+      let xml = FileManager.default.contents(atPath: path),
+      let config = try? PropertyListDecoder().decode(Config.self, from: xml)
+    else {
+      return Config(apiUrl: "")
+    }
+    return config
+  }()
+  private(set) lazy var apollo = ApolloClient(url: URL(string: self.config.apiUrl)!)
+}
