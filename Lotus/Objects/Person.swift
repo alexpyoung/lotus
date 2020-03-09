f0b75bc984d26a3ea4aace84efc6c473331c0cfd
Alex Young <alexyoung1992@gmail.com>
2020-03-09 18:43:27 -0400

    feat(Person): init with keychain

--- Lotus/Objects/Person.swift
+++ Lotus/Objects/Person.swift
@@ -4,4 +4,8 @@ class Person: ObservableObject {
   // swiftlint:disable identifier_name
   @Published var id: String?
   @Published var auth0id: String?
+
+  init() {
+    self.auth0id = Authentication.shared.decoded?.body["sub"] as? String
+  }
 }
