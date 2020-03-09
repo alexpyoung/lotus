86e9d2063c5fec6e6715f1f7adf3e32f7ec2082d
Alex Young <alexyoung1992@gmail.com>
2020-03-08 20:48:25 -0400

    feat(Network): send JWT in headers

--- Lotus/Services/Network.swift
+++ Lotus/Services/Network.swift
@@ -12,18 +12,40 @@ class Network {
       let path = Bundle.main.path(forResource: "Apollo", ofType: "plist"),
       let xml = FileManager.default.contents(atPath: path),
       let config = try? PropertyListDecoder().decode(Config.self, from: xml)
-    else {
-      return nil
+      else {
+        return nil
     }
     return config
   }()
-  private(set) lazy var apollo: ApolloClient! = {
+  private lazy var transport: NetworkTransport? = {
     guard
       let apiUrl = self.config?.apiUrl,
       let url = URL(string: apiUrl)
-    else {
-      return nil
+      else {
+        return nil
     }
-    return ApolloClient(url: url)
+    let transport = HTTPNetworkTransport(url: url)
+    transport.delegate = self
+    return transport
   }()
+
+  private(set) lazy var apollo: ApolloClient! = {
+    guard let transport = self.transport else { return nil }
+    return ApolloClient(networkTransport: transport)
+  }()
+}
+
+extension Network: HTTPNetworkTransportPreflightDelegate {
+  func networkTransport(_ networkTransport: HTTPNetworkTransport, shouldSend request: URLRequest) -> Bool {
+    Authentication.shared.jwt != nil
+  }
+
+  func networkTransport(_ networkTransport: HTTPNetworkTransport, willSend request: inout URLRequest) {
+    guard let token = Authentication.shared.jwt else {
+      return
+    }
+    var headers = request.allHTTPHeaderFields ?? [String: String]()
+    headers["Authorization"] = "Bearer \(token)"
+    request.allHTTPHeaderFields = headers
+  }
 }
