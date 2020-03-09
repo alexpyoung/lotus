0df3dce6c4bb6168936c7b730146e88c4a83687a
Alex Young <alexyoung1992@gmail.com>
2020-03-09 00:17:06 -0400

    feat(fastlane): setup build and test

--- LotusUITests/LotusUITests.swift
+++ LotusUITests/LotusUITests.swift
@@ -1,34 +1,3 @@
-//
-//  LotusUITests.swift
-//  LotusUITests
-//
-//  Created by Alex Young on 3/7/20.
-//  Copyright © 2020 Alex Young. All rights reserved.
-//
-
 import XCTest
 
-class LotusUITests: XCTestCase {
-
-    override func setUp() {
-      self.continueAfterFailure = false
-    }
-
-    func testExample() {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testLaunchPerformance() {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
-                XCUIApplication().launch()
-            }
-        }
-    }
-}
+class LotusUITests: XCTestCase {}
--- README.md
+++ README.md
@@ -6,8 +6,20 @@
 yarn install
 ```
 
+### Build
+```
+bundle install
+bundle exec fastlane ios build
+```
+
+### Test
+```
+bundle install
+bundle exec fastlane ios test
+```
+
 ### Add Dependency
 ```
 bundle install
-bundle exec pod install
+bundle exec fastlane ios install
 ```
