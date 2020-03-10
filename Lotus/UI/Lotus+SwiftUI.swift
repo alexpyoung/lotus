9cb5ae99828e72ed4c5541930e9eeebd1151416d
Alex Young <alexyoung1992@gmail.com>
2020-03-09 23:17:25 -0400

    feat(Color): add brand colors

--- Lotus/UI/Lotus+SwiftUI.swift
+++ Lotus/UI/Lotus+SwiftUI.swift
@@ -11,6 +11,10 @@ extension Color {
 
   static let offBlack = Color(red: 26/255, green: 26/255, blue: 26/255)
   static let grayLight = Color(red: 133/255, green: 145/255, blue: 155/255)
+  static let navy = Color(red: 33/255, green: 36/255, blue: 61/255)
+  static let salmon = Color(red: 1, green: 124/255, blue: 124/255)
+  static let mustard = Color(red: 1, green: 208/255, blue: 130/255)
+  static let aqua = Color(red: 136/255, green: 225/255, blue: 242/255)
 }
 
 extension View {
